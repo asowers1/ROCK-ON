fed1dcee07220f092ee2f5fee45650c966d106a3
asowers1 <asow123@gmail.com>
2015-09-24 23:24:37 -0400

    added cocoapods

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+
+use_frameworks!
+target 'ROCK ON' do
+pod 'RealmSwift'
+end
+
+target 'ROCK ONTests' do
+pod 'RealmSwift'
+end
+
+target 'ROCK ONUITests' do
+pod 'RealmSwift'
+end
+
---
+++ ROCK ONTests/ROCK_ONTests.swift
@@ -0,0 +1,36 @@
+//
+//  ROCK_ONTests.swift
+//  ROCK ONTests
+//
+//  Created by Andrew Sowers on 9/23/15.
+//  Copyright © 2015 Andrew Sowers. All rights reserved.
+//
+
+import XCTest
+@testable import ROCK_ON
+
+class ROCK_ONTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
